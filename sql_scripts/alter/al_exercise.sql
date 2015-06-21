3ea3b5dce576b78e3bc069080cd30fccb2d8bf1f
igoryen <igor.yentaltsev@gmail.com>
2015-06-21 17:15:33 -0400

    Can insert rows into Exercises without id

--- sql_scripts/alter/al_exercise.sql
+++ sql_scripts/alter/al_exercise.sql
@@ -1,4 +1,9 @@
 ALTER TABLE `bearberry`.`exercise` 
+CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT ,
+ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `bearberry`.`exercise` 
 CHANGE COLUMN `name` `abbreviation` 
 VARCHAR(500) NOT NULL COMMENT 'the name of the exercise' ,
 CHANGE COLUMN `abbreviation` `name` 
--- sql_scripts/insert/in_exercise.sql
+++ sql_scripts/insert/in_exercise.sql
@@ -1,13 +1,13 @@
 # adding a new exercise to the database
-INSERT INTO bearberry.exercise (id, abbreviation, `name`, `force`, main_muscle, other_muscle, description) 
+INSERT INTO bearberry.exercise (abbreviation, `name`, `force`, main_muscle, other_muscle, description) 
 VALUES
-(102, 
-'dc', 
-'Decline Crunch', 
-'abs', 
-'abs',
-null, 
-'Shoulders come up about 10 cm, lower back remains on the bench');
+(
+'test', 
+'Test', 
+'test', 
+'test',
+'', 
+'test test');
 
 SELECT * 
 FROM bearberry.exercise 
